5cf637ed537994237f7c266bb8582940005edbcf
zalan <zlaval@gmail.com>
2019-03-11 23:00:40 +0100

    init repository

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.21"
+}
+
+group = "com.zlrx.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.1.1")
+
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
---
+++ src/main/kotlin/com/zlrx/kotlin/coroutines/SimpleCoroutine.kt
@@ -0,0 +1,30 @@
+package com.zlrx.kotlin.coroutines
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+const val THRESHOLD = 5000
+
+suspend fun compute(array: IntArray, low: Int, high: Int): Long {
+    return if (high - low <= THRESHOLD) {
+        (low until high)
+            .map { array[it].toLong() }
+            .sum()
+    } else {
+        val mid = low + (high - low) / 2
+        val left = GlobalScope.async { compute(array, low, mid) }
+        val right = compute(array, mid, high)
+        return left.await() + right
+    }
+}
+
+fun main(args: Array<String>) = runBlocking {
+    val list = (0..20_000_000).toList().toIntArray()
+    var result = 0L
+    val time = measureTimeMillis {
+        result = compute(list, 0, list.size)
+    }
+    println("$result in $time ms")
+}
